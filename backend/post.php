beb1df0c00bbc91d87dfdbf4c37edbc09659020d
ctapiadev <ctapiadev@gmail.com>
2024-04-13 04:29:41 -0400

    add validations php, add alert warning and check js html

--- backend/post.php
+++ backend/post.php
@@ -1,14 +1,52 @@
 <?php
 
 include_once 'conexion.php';
+include_once 'funciones.php';
 
 if ($_POST) {
 
+    // Variables
     $nombre = $_POST['vNombre'];
+    $alias = $_POST['vAlias'];
+    $rutN = $_POST['vRut'];
+    $mail = $_POST['vMail'];
+    $region = $_POST['vRegion'];
+    $comuna = $_POST['vComuna'];
+    $candidato = $_POST['vCandidato'];
+    $web = $_POST['vWeb'];
+    $tv = $_POST['vTV'];
+    $redes = $_POST['vRedes'];
+    $amigo = $_POST['vAmigo'];
 
-    $sql_add = 'INSERT INTO user (nombre) VALUES (?)';
-    $sentencia_agregar = $pdo->prepare($sql_add);
-    $sentencia_agregar->execute(array($nombre));
+    //Validaciones
+    if ($nombre === '' || $alias === '' || $rutN === '' || $mail === '' || $region === '---' || $comuna === '---' || $candidato === '---') {
+        echo json_encode(0);
+    } else if (!validarEmail($mail) && validarRut($rutN)) {
+        echo json_encode(1);
+    } else if (!validarEmail($mail)) {
+        echo json_encode(1);
+    } else if (!validarRut($rutN) && validarEmail($mail)) {
+        echo json_encode(2);
+    } else if (!validarRut($rutN)) {
+        echo json_encode(2);
+    } else {
+        echo json_encode('Todo Valido');
+    }
 
-    echo json_encode($nombre." agregado");
+
+
+    
+
+    
+
+
+
+
+    // $sql_add = 'INSERT INTO user (nombre) VALUES (?)';
+    // $sentencia_agregar = $pdo->prepare($sql_add);
+    // $sentencia_agregar->execute(array($nombre));
+
+    
 }
+
+
